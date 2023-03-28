7948252cd5288cd6a35c52b095c341a2b7daae73
kaannu <kaannu@ttu.ee>
2023-03-28 05:54:12 +0300

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="bg-white">
+    <div class="bg-black">
+      <div class="text-gray-400">Page</div>
+    </div>
+    &nbsp;
+    <div class="flex justify-center mb-8"><img src="./assets/img/inbank.png" alt="Inbank"></div>
+    <div class="absolute top-10 right-20 h-10 w-12"><svg fill="none" stroke="currentColor" stroke-width="1.5"
+        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75">
+        </path>
+      </svg></div>
+
+
+    <div class="flex justify-center flex-wrap mr-10"> <!--see paneb kogu klaustri pic + text keskele-->
+      <div class="relative">
+        <img class="rectangle" src="./assets/img/rectangle.png" alt="Rectangle">
+        <img class="cupcake absolute top-30 left-35 flex" src="./assets/img/cupcake.png" alt="Cupcake">
+      </div>
+
+      <div class="pl-36"> <!--teksti ja pildi vahe suurus, vahe suuruse muutmiseks kasuta md v lg-d jne-->
+        <div class="grid gap-1 grid-cols-1 grid-rows-2 text-center mt-12">
+          <span class="text-4xl">Good news!</span>
+          <span class="text-lg text-gray-500">Your loan has been approved</span>
+        </div>
+        &nbsp;
+        <div class="border-t-1 border border-gray-130"></div>
+        &nbsp;
+        <div
+          class="grid items-end gap-y-10 grid-cols-2 grid-rows-3 text-center text-gray-500 px-2 border-gray-200 h-32 justify-center">
+          <span>Personal loan offer</span>
+          <span class="text-black text-lg">1,700.00 €</span> <!--siia lisada dollar sign-->
+          <span>Loan period</span>
+          <span class="text-black text-lg">36 months</span><!--months peab väiksem olema kui 36-->
+          <span>Monthly payment</span>
+          <span class="text-black text-lg">92.27%</span>
+        </div>
+        &nbsp;
+        <div class="border-t-1 border border-gray-130"></div>
+        &nbsp;
+        <div class="grid gap-10 grid-cols-2 grid-rows-2 text-center text-gray-500 border-gray-500 mb-7">
+          <span>Interest rate</span>
+          <span class="text-black text-lg">12.90%</span>
+          <span>Margin</span>
+          <span class="text-black text-lg">8.90% + 6 months EURIBOR (0%)</span>
+        </div>
+        <div class="border-t-1 border border-gray-130 text-center border-gray-200"></div>
+        &nbsp;
+        <div class="flex items-center justify-center text-gray-500 text-center">
+          <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true" style="width: 20; height: 20;">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+          </svg>
+          &nbsp; Promo code applied
+        </div>
+
+
+        &nbsp;
+        <div class="border-t-1 border border-gray-130"></div>
+        &nbsp;
+        <div class="grid grid-cols-1 grid-rows-2 gap-5">
+          <button class=" btn rounded bg-purple-900 text-white cursor-pointer">View offer details</button>
+          <div class>
+            <Popup :is-open="popupOpen" @close="popupOpen = false">
+            </Popup>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Popup from './components/Popup.vue';
+
+
+export default {
+  components: {
+    Popup
+  },
+  data() {
+    return {
+      popupOpen: false
+    };
+  },
+  methods: {
+    showPopup() {
+      this.popupOpen = true;
+    }
+  }
+};
+</script>
---
+++ vue-project/src/components/Popup.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <button class="btntwo w-rounded bg-white cursor-pointer semi-bold" @click="toggleModal = !toggleModal">
+      Not interested
+    </button>
+    <!--popup sisu-->
+    <div class="fixed inset-0 flex justify-center items-center z-50" v-if="toggleModal">
+      <div class="bg-white rounded-xl w-96 h-auto px-5 py-4 flex flex-col">
+        <!--X nupp-->
+        <button class="text-lg text-gray-300 hover:text-gray-900 cursor-pointer" @click="toggleModal = false">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+        &nbsp;
+        <div class="text-lg text-left mt-4 px-3 text-gray-700 font-semibold">Help us improve!</div>
+        <p class="text-sm text-left mt-2 mb-3 px-3 text-gray-600">We want to offer our clients the best experience. Your
+          feedback will help us get better. You can select multiple answers.</p>
+        <div class="flex flex-col items-start">
+          <label class="inline-flex items-center text-center mb-6" v-for="(option, index) in options" :key="index">
+            <input type="checkbox" class="form-checkbox" :value="option" v-model="selectedOptions">
+            <span class="text-sm flex justify-center items-center">{{ option }}</span>
+          </label>
+        </div>
+        <button class="btn" @click="toggleModal = false">
+          Cancel loan application
+        </button>
+      </div>
+    </div>
+    <div v-if="toggleModal" class="fixed inset-0 backdrop-blur bg-opacity-25"></div>
+  </div>
+</template>
+  
+<script>
+export default {
+  name: "modal",
+  data() {
+    return {
+      toggleModal: false,
+      options: [
+        "Loan amount is too small",
+        "Interest is too high",
+        "Not sure if I should take a loan",
+        "I prefer another bank",
+        "Option 5",
+        "Timing is not right",
+        "Other"
+      ],
+      selectedOptions: []
+    };
+  }
+};
+</script>
+
+   
+  
+  
